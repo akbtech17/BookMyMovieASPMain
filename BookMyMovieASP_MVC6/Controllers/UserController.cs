da57ad84ac6918f4c4ff1249ee270111c7e37d01
anshul bansal <akbt.tech17@gmail.com>
2022-09-28 20:17:59 +0530

    added sign in view and register view

---
+++ BookMyMovieASP_MVC6/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookMyMovieASP_MVC6.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult Register() 
+        {
+            return View();
+        }
+    }
+}
